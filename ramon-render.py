e5080ad97e7da0652010bf4cb126bb2dbd553234
Guido Martínez <mtzguido@gmail.com>
2023-06-03 19:36:54 -0700

    crappy vertical labels on marks

--- ramon-render.py
+++ ramon-render.py
@@ -1,35 +1,57 @@
 #!/usr/bin/env python3
 
 from parse import *
-import matplotlib
 
 def load_file(fn):
     loads = {}
+    marks = {}
     with open(fn) as f:
         for line in f:
             if not (search("poll" , line)):
                 continue
-            wall = search("wall={:g}", line).fixed
-            usage = search("usage={:g}", line).fixed
-            load = search("load={:g}", line).fixed
+            wall = search("wall={:g}", line).fixed[0]
+            usage = search("usage={:g}", line).fixed[0]
+            load = search("load={:g}", line).fixed[0]
             loads[wall] = load
-    return loads, 1
+    with open(fn) as f:
+        for line in f:
+            if not (search("mark" , line)):
+                continue
+            mark = search("str={:S}", line).fixed[0]
+            wall = search("wall={:g}", line).fixed[0]
+            marks[mark] = wall
 
-def plot(fn, load):
+    return loads, marks
+
+def plot(fn, loads, marks):
     import matplotlib.pyplot as plt
     import numpy as np
 
     x_axis = []
     y_axis = []
-    for t in load.keys():
+    for t in loads.keys():
         x_axis.append(t)
-        y_axis.append(load[t])
+        y_axis.append(loads[t])
 
     plt.plot(x_axis, y_axis)
 
+    lasttime = -10
+    lasty = -5
+    for tag in marks.keys():
+        time = marks[tag]
+        if (lasttime + 20 >= time):
+            y = lasty + 1
+        else:
+            y = -5
+        plt.vlines(time, -5, 29, colors='C1', label=tag);
+        plt.text(time, y, tag, rotation=45, size='smaller')
+        lasttime = time
+        lasty = y
+    #plt.legend()
+
     plt.xlabel("Wall clock time")
     plt.ylabel("Load")
-
+    
     plt.title("Load average across time")
 
     imagefn = fn + ".png"
@@ -46,8 +68,8 @@ def main():
 
     file = args.file
 
-    load, totaltime = load_file(file)
+    loads, marks = load_file(file)
 
-    plot(file, load)
+    plot(file, loads, marks)
 
 main()
